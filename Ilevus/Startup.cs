1f0997d8594c4f0845ee7cb74cad2b70007a604c
ivan.feliciano <ivan.feliciano@mereoconsulting.com.br>
2016-02-29 16:26:55 -0300

    Iniciando projeto em MVC, primeiro commit do projeto zerado.

---
+++ Ilevus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ilevus.Startup))]
+namespace Ilevus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
