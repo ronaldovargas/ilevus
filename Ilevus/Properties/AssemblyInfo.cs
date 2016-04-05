06d9cdb70d3fc62fd49c424fc058ad3f27dde451
ivan.feliciano <ivan.feliciano@mereoconsulting.com.br>
2016-04-05 17:02:20 -0300

    Migração para Grupo de Projeto

--- Ilevus/Properties/AssemblyInfo.cs
+++ Ilevus/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ilevus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ilevus")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ilevus")]
 [assembly: AssemblyProduct("Ilevus")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
--- utils/Readme.txt
+++ utils/Readme.txt
@@ -1,2 +1,6 @@
 MVC 5 - https://www.youtube.com/watch?v=zWFoZb6EiwU
 
+Código fonte para Chat, vídeo online: 
+Link: https://vcca.codeplex.com
+Fonte em C#: vcca-bbad5c99ccbd.zip, 
+PDF: Collaborative _Video_Application_Setup_Documentation_Open_Source_App.pdf
