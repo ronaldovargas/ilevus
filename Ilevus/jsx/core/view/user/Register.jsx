18a72fca515c1f0e110bf41442479bed44d2bc22
Renato R. R. de Oliveira <renatorro@comp.ufla.br>
2016-05-22 02:18:36 -0300

    Tela de cadastro

--- Ilevus/jsx/core/view/user/Register.jsx
+++ Ilevus/jsx/core/view/user/Register.jsx
@@ -16,34 +16,7 @@ module.exports = React.createClass({
         router: React.PropTypes.object
     },
     getInitialState() {
-		return {
-			fields: [{
-			    name: "FirstName",
-			    type: "text",
-			    placeholder: "",
-			    label: Messages.get("LabelFirstName")
-			},{
-			    name: "LastName",
-			    type: "text",
-			    placeholder: "",
-			    label: Messages.get("LabelLastName")
-			},{
-			    name: "Email",
-			    type: "email",
-			    placeholder: "",
-			    label: Messages.get("LabelEmail")
-			},{
-				name: "Password",
-				type: "password",
-				placeholder: "",
-				label: Messages.get("LabelPassword")
-			}, {
-			    name: "ConfirmPassword",
-			    type: "password",
-			    placeholder: "",
-			    label: Messages.get("LabelPasswordConfirm")
-			}]
-		};
+        return {};
 	},
 	onSubmit(data) {
 	    UserStore.dispatch({
@@ -62,41 +35,72 @@ module.exports = React.createClass({
 	    UserStore.off(null, null, this);
 	},
 	render() {
-		return (
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col-xs-12 text-center">
-						<div className="ilevus-login-header">
-							<img className="ilevus-login-brand" src={AppLogo} alt="ilevus Logo" />
-							<h1>Faça seu cadastro</h1>
-						</div>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-md-4 col-md-offset-4">
-						<div className="ilevus-card">
-							<div className="ilevus-login-body">
-								<VerticalForm
-									onSubmit={this.onSubmit}
-									fields={this.state.fields}
-									store={UserStore}
-									submitLabel="Realizar Cadastro"
-									blockButtons={true}
-								/>
-							</div>
+		return (<div className="container">
+                <div className="row">
+                  <div className="col-xs-12 col-sm-8 col-sm-offset-2 col-xl-6 col-xl-offset-3">
+                    <div className="p-y-3 text-xs-center">
+                      <img src={AppLogo} />
+                    </div>
+                    <form className="p-t-3">
+                      <div className="m-b-3 text-sm-center">
+                        <h2>Crie sua conta agora</h2>
+                        <p className="m-y-0">Faça seu cadastro em segundos, não precisa de cartão.</p>
+                        <p>Já tem um cadastro na Ilevus? <Link to="/login">Faça seu login</Link></p>
+                      </div>
+                      <div className="form-group row">
+                          <div className="col-xs-12 col-sm-6">
+                            <label className="form-element-label" htmlFor="name">{Messages.get("LabelName")}</label>
+                            <input className="form-element" id="name" name="name" type="text" />
+                          </div>
+                          <div className="col-xs-12 col-sm-6">
+                            <label className="form-element-label" htmlFor="surname">{Messages.get("LabelSurname")}</label>
+                            <input className="form-element" id="surname" name="surname" type="text" />
+                          </div>
+                      </div>
+                      <div className="form-group">
+                        <label className="form-element-label" htmlFor="email">{Messages.get("LabelEmail")}</label>
+                        <input className="form-element" id="email" name="email" type="text" />
+                      </div>
+                      <div className="form-group">
+                        <label className="form-element-label" htmlFor="phoneNumber">{Messages.get("LabelPhone")}</label>
+                        <input className="form-element" id="phoneNumber" name="phoneNumber" type="tel" />
+                      </div>
+                      <div className="form-group">
+                        <label className="form-element-label" htmlFor="sex">{Messages.get("LabelSex")}</label>
+                        <input className="form-element" id="sex" name="sex" type="text" />
+                      </div>
+                      <div className="form-group">
+                        <label className="form-element-label" htmlFor="address">{Messages.get("LabelAddress")}</label>
+                        <input className="form-element" id="address" name="address" type="text" />
+                      </div>
+                      <div className="form-group row">
+                          <div className="col-xs-12 col-sm-6">
+                              <label className="form-element-label" htmlFor="password">{Messages.get("LabelPassword")}</label>
+                              <input className="form-element" id="password" name="password" type="password" />
+                          </div>
+                          <div className="col-xs-12 col-sm-6">
+                              <label className="form-element-label" htmlFor="passwordconfirm">{Messages.get("LabelPasswordConfirm")}</label>
+                              <input className="form-element" id="passwordconfirm" name="passwordconfirm" type="text" />
+                          </div>
+                      </div>
+                      <p className="small">
+                          Ao clicar em "Criar minha conta", você está de acordo com nossa
+                          Política de Privacidade e Termos de Uso.
+                      </p>
+                      <div className="form-group row">
+                        <div className="col-xs-12 col-sm-6">
+                          <input type="submit" value="Criar minha conta" className="btn btn-brand btn-block" />
+                        </div>
+                      </div>
+                    </form>
 
-							<div className="ilevus-login-footer">
-								<div className="row">
-									<div className="col-md-12 text-center">
-										<Link to="/login">Já é cadastrado? Faça seu login.</Link>
-									</div>
-									<br/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
+                    <div className="text-xs-center">
+                      <p className="text-muted small p-t-2">
+                          ©2016 Ilevus. Todos os direitos reservados.
+                      </p>
+                    </div>
+                  </div>
+              </div>
+		</div>);
 	}
 });
