e53b4270457fde6962977588208bec303db8cd89
henriquehfr <henriquehfr2@gmail.com>
2016-05-22 02:41:44 -0300

    Correcoes da navbar

--- Ilevus/jsx/core/widget/TopBar.jsx
+++ Ilevus/jsx/core/widget/TopBar.jsx
@@ -7,30 +7,30 @@ var Modal = require("ilevus/jsx/core/widget/Modal.jsx");
 var LogoWhite = require('ilevus/img/ilevus-logo-white-20px.png');
 
 module.exports = React.createClass({
-	getInitialState() {
-		return {
-			user: UserSession.get("user"),
-			logged: !!UserSession.get("logged")
-		};
-	},
-	componentWillMount() {
-		var me = this;
-		UserSession.on("login", session => {
-			me.setState({
-				user: session.get("user"),
-				logged: true
-			});
-		}, me);
-		UserSession.on("logout", session => {
-			me.setState({
-				user: null,
-				logged: false
-			});
-		}, me);
-	},
-	componentWillUnmount() {
-		UserSession.off(null, null, this);
-	},
+    getInitialState() {
+        return {
+            user: UserSession.get("user"),
+            logged: !!UserSession.get("logged")
+        };
+    },
+    componentWillMount() {
+        var me = this;
+        UserSession.on("login", session => {
+            me.setState({
+                user: session.get("user"),
+                logged: true
+            });
+        }, me);
+        UserSession.on("logout", session => {
+            me.setState({
+                user: null,
+                logged: false
+            });
+        }, me);
+    },
+    componentWillUnmount() {
+        UserSession.off(null, null, this);
+    },
     onLogout(evt) {
         evt.preventDefault();
         UserSession.dispatch({
@@ -38,40 +38,49 @@ module.exports = React.createClass({
         });
     },
     onSearch(evt) {
-    	evt.preventDefault();
-    	location.assign("#/search/"+encodeURI(this.refs['search-term'].value));
+        evt.preventDefault();
+        location.assign("#/search/" + encodeURI(this.refs['search-term'].value));
     },
 
-	render() {
-	    return (<nav className="navbar navbar-fixed-top navbar-dark bg-primary">
+    render() {
+        return (<nav className="navbar navbar-fixed-top navbar-dark bg-primary">
           <div className="container">
             <a className="navbar-brand" href="#/">
               <img src={LogoWhite} />
             </a>
-            <button className="navbar-toggler hidden-sm-up pull-right-xs" type="button" data-toggle="collapse" data-target="#js-navbar-collapse">&#9776;</button>
+            <button className="navbar-toggler hidden-sm-up pull-xs-right" type="button" data-toggle="collapse" data-target="#js-navbar-collapse">&#9776;</button>
             <div className="collapse navbar-toggleable-xs" id="js-navbar-collapse">
-                <form className="form-inline" onSubmit={this.onSearch}>
-                  <input ref="search-term" className="form-element form-element-sm" type="text" />
-                  <button className="btn btn-sm btn-neutral" type="submit">Buscar</button>
+                <form className="form-inline pull-sm-left" onSubmit={this.onSearch}>
+                  <input ref="search-term" className="form-element" type="text" />
+                  <button className="btn btn-brand" type="submit">Buscar</button>
                 </form>
                 {this.state.logged ? (
-                <div className="navbar-nav">
-                    <span className="nav-item" style={{
-			                marginRight: '5px',
-			                height: '50px',
-			                width: '50px',
-			                borderRadius: '100%',
-			                backgroundPosition: 'center',
-                            backgroundSize: 'auto 100%',
-			                backgroundImage: "url(" + this.state.user.Image+")"
-			            }} />
-			        <span className="nav-item">{this.state.user.Name}</span>
-			        <span className="nav-item" title="Notificações">Notificações</span>
-			        <a className="nav-item" onClick={this.onLogout}>
-				        <span title="Sair">Logout</span>
-			        </a>
-                </div>
-                ):""}
+                <ul className="nav navbar-nav pull-sm-right">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <span className="avatar avatar-xs m-r-1">
+                              <img className="img-fluid" src={this.state.user.Image} alt={this.state.user.Name} />
+                            </span>
+                            <span>{this.state.user.Name}</span>
+                        </a>
+                    </li>
+			        <li className="nav-item" title="Notificações">
+                        <a className="nav-link" href="#">Notificações</a>
+                    </li>
+			        <li className="nav-item">
+				        <a className="nav-link" href="#" onClick={this.onLogout}>Logout</a>
+			        </li>
+                </ul>
+                ) : (
+                <ul className="nav navbar-nav pull-sm-right">
+			        <li className="nav-item">
+                        <a className="nav-link" href="#/signup">Cadastrar</a>
+                    </li>
+			        <li className="nav-item">
+				        <a className="nav-link" href="#/login">Entrar</a>
+			        </li>
+                </ul>
+                )}
             </div>
           </div>
         </nav>);
@@ -92,5 +101,5 @@ module.exports = React.createClass({
 				    <span title="Sair" className="mdi mdi-logout" />
 			    </a>
 		</div>);*/
-	  }
-	});
+    }
+});
