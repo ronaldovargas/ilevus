a204f449f53adfc7d663cb7203c11ec6f4411ba4
henri <henriquehfr2@gmail.com>
2016-09-13 09:39:14 -0300

    Adicionado mock do painel de administracao e da tela de traducoes

---
+++ Ilevus/jsx/core/view/admin/AdminPanel.jsx
@@ -0,0 +1,37 @@
+﻿
+var React = require("react");
+var Link = require("react-router").Link;
+
+var Messages = require("ilevus/jsx/core/util/Messages.jsx");
+
+module.exports = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object
+    },
+
+    render() {
+        return (
+            <div className="container p-y-3">
+                <div className="row">
+                    <div className="col-xs-2 m-b-1">
+                        <ul className="ilv-nav ilv-nav-stacked ilv-nav-menu">
+                            <li className="ilv-nav-item">
+                                <Link className="ilv-nav-link" to="/admin/users">
+                                    Usuários
+                                </Link>
+                            </li>
+                            <li className="ilv-nav-item">
+                                <Link className="ilv-nav-link" to="/admin/translate">
+                                    Traduções
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-xs-10">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
---
+++ Ilevus/jsx/core/view/admin/AdminPanelTranslate.jsx
@@ -0,0 +1,52 @@
+﻿var React = require("react");
+var Link = require("react-router").Link;
+var UserSession = require("ilevus/jsx/core/store/UserSession.jsx");
+var Messages = require("ilevus/jsx/core/util/Messages.jsx");
+
+module.exports = React.createClass({
+    render () {
+        return (
+            <div>
+                <div className="ilv-card">
+                    <div className="ilv-card-header">
+                        <div className="ilv-media">
+                            <div className="ilv-media-body">
+                                <strong>Traduções</strong>
+                            </div>
+                            <div className="ilv-media-left">
+                                <button className="ilv-btn ilv-btn-link">Adicionar idioma</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ilv-card-body">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Código</th>
+                                    <th>PT_BR</th>
+                                    <th>EN_US</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>LabelName</td>
+                                    <td>Nome</td>
+                                    <td>Name</td>
+                                    <td>
+                                        <button className="ilv-btn ilv-btn-clean ilv-btn-icon" title="Editar Entrada">
+                                            <i className="ilv-icon material-icons">&#xE3C9;</i>
+                                        </button>
+                                        <button className="ilv-btn ilv-btn-clean ilv-btn-icon" title="Excluir Tradução">
+                                            <i className="ilv-icon material-icons">&#xE872;</i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
