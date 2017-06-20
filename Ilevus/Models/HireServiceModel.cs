63517f018028e24b08889126faa95badd2904d05
gnios <eugenio00@gmail.com>
2017-06-20 15:48:04 -0300

    Adicionando moip

---
+++ Ilevus/Models/HireServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ilevus.Models
+{
+	public class HireServiceModel
+	{
+		public IList<UserService> Services { get; set; }
+	}
+}
