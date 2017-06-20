e35105b238c21a071c69cf2dc8fc586fd6199fcc
gnios <eugenio00@gmail.com>
2017-06-20 15:48:04 -0300

    Adicionando moip

--- Ilevus/Controllers/FinancialController.cs
+++ Ilevus/Controllers/FinancialController.cs
@@ -15,6 +15,8 @@ using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Moip.Net.V2;
+using Moip.Net.V2.Model;
 using static ilevus.Models.CoachingSession;
 
 namespace ilevus.Controllers
@@ -111,5 +113,89 @@ namespace ilevus.Controllers
         }
 
 
-    }
+	    [HttpPost]
+	    [Route("HireService")]
+	    public async Task<IHttpActionResult> HireService(HireServiceModel model)
+	    {
+		    var db = IlevusDBContext.Create();
+		    try
+		    {
+			    var user = await UserManager.FindByNameAsync(User.Identity.Name);
+			    if (user == null)
+			    {
+				    return BadRequest("You must be logged in.");
+			    }
+
+			    var v2Client = new V2Client(
+				    new Uri("https://sandbox.moip.com.br/"),
+					"QXEYNX1WIEGI3VZV9IBKU0ZZQCIKU4BF",
+					"6NU0BEALWOVLIVR4FD1FRIGTE6NONJYJOESOBT5N"
+				);
+
+			    var pedido = new Pedido()
+			    {
+				    OwnId = "SEU_CODIGO_PEDIDO",
+				    Amount = new Valores()
+				    {
+					    Currency = CurrencyType.BRL,
+					    Subtotals = new Subtotal()
+					    {
+						    Shipping = 1000
+					    }
+				    },
+				    Items = new List<ItemPedido>()
+				    {
+					    new ItemPedido()
+					    {
+						    Product = "Descrição do produto",
+						    Quantity = 1,
+						    Detail = "Detalhes",
+						    Price = 1000
+					    }
+				    },
+				    Customer = new Cliente()
+				    {
+					    OwnId = "SEU_ID_CLIENTE",
+					    Fullname = "José Silva",
+					    Email = "josesilva@acme.com.br",
+					    BirthDate = DateTime.Now.Date.AddYears(-18).ToString("yyyy-MM-dd"),
+					    TaxDocument = new Documento()
+					    {
+						    Type = DocumentType.CPF,
+						    Number = "65374721054"
+					    },
+					    Phone = new Telefone()
+					    {
+						    CountryCode = 55,
+						    AreaCode = 11,
+						    Number = 999999999
+					    },
+					    ShippingAddress = new Endereco()
+					    {
+						    ZipCode = "01234000",
+						    Street = "Avenida Faria Lima",
+						    StreetNumber = "2927",
+						    Complement = "SL 1",
+						    City = "São Paulo",
+						    District = "Itaim",
+						    State = "SP",
+						    Country = "BRA"
+					    }
+				    }
+			    };
+
+			    var clienteCriado = v2Client.CriarPedido(pedido);
+
+			await UserManager.UpdateAsync(user);
+
+			    return Ok();
+		    }
+		    catch (Exception e)
+		    {
+			    return InternalServerError(e);
+		    }
+	    }
+
+
+	}
 }
