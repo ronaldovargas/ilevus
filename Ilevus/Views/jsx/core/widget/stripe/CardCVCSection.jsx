182a71bccac977b7def78f81a7d92cf091ecacf9
Eugenio Tavares <eugenio00@gmail.com>
2017-10-19 21:34:55 -0300

    'migracao'

---
+++ Ilevus/Views/jsx/core/widget/stripe/CardCVCSection.jsx
@@ -0,0 +1,24 @@
+var CardCVCElement = require('react-stripe-elements/lib/index').CardCVCElement;
+var CardExpiryElement = require('react-stripe-elements/lib/index').CardExpiryElement;
+module.exports = React.createClass({
+
+  render() {
+    return (
+
+      <div className="row">
+        <div className="col-6">
+          <div className="ilv-form-group">
+            <label for="cc-payment" className="ilv-control-label mb-1">Payment amount</label>
+            <CardCVCElement className="ilv-form-control"/>
+          </div>
+        </div>
+        <div className="col-6">
+        <div className="ilv-form-group">
+          <label for="cc-payment" className="ilv-control-label mb-1">Payment amount</label>
+          <CardExpiryElement className="ilv-form-control"/>
+        </div>
+      </div>
+      </div>
+    );
+  }
+});
